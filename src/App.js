8544a17bf9287dd0da2d530ad1bcec3120e78e1c
Adel Lassag <adetech.aadl@gmail.com>
2018-08-30 18:55:33 +0100

    feat: import menu component to main App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Menu from './Menu';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Menu />
       </div>
     );
   }
