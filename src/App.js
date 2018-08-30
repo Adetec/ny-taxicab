b73c90de5c7928199755eacc800c5dbddda6fb1e
Adel Lassag <adetech.aadl@gmail.com>
2018-08-30 22:24:28 +0100

    feat: Create lifecycle event

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ class App extends Component {
   state = {
     places : []
   }
+  componentDidMount() {
+    this.retrieveVenues();
+  }
 
   retrieveVenues = () => {
     const endPoint = 'https://api.foursquare.com/v2/venues/explore?';
@@ -19,7 +22,7 @@ class App extends Component {
       client_id : 'KGRM5HI3USNJX2LWHTWJ1J5PPJEF3F5LJP31GCLUATCK41AP',
       client_secret : 'KKZWIDKWEICCGARKXIEZJFZOTJASTXJ33NUZJBM5QUYZO3AM',
       v : '20183008',
-      near : 'mountain view'
+      near : 'New York'
     }
     axios.get(endPoint + new URLSearchParams(parameters)).then(response => {
       this.setState({
