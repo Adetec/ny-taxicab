a5fc39cfa10af8ffa712234ae3cb8a6a51ba5fc9
Adel Lassag <adetech.aadl@gmail.com>
2018-08-31 00:06:01 +0100

    refactor: Create simple map component

--- src/Map.js
+++ src/Map.js
@@ -1,33 +1,13 @@
 import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
 
 class Map extends Component {
-  static defaultProps = {
-    center: {
-      lat: 40.712775,
-      lng: -74.005973
-    },
-    zoom: 11
-  };
+
 
   render() {
     return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyCYdaUFGD_xCrOUrnEmg-JjfzX9Ikt6BB8', language : 'eng' }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={this.props.center.lat}
-            lng={this.props.center.lng}
-            text={'New York'}
-          />
-        </GoogleMapReact>
-      </div>
+      <div id="map"></div>
     );
   }
 }
