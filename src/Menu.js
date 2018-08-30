7cbf1ab12458d095a4ae5eaf88d799ebf3ce8361
Adel Lassag <adetech.aadl@gmail.com>
2018-08-30 18:54:20 +0100

    feat: Create menu component

---
+++ src/Menu.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Menu extends Component {
+    render() {
+      return (
+        <aside className="menu">
+  
+        </aside>
+      );
+    }
+}
+  
+export default Menu;
