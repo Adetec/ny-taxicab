e041f070d2522187aee769153fa96962d4c3c5f9
Adel Lassag <adetech.aadl@gmail.com>
2018-08-30 22:23:36 +0100

    feat: Create methode that fetch data from 3party

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import './App.css';
 import Menu from './Menu';
 import Navbar from './Navbar';
@@ -9,6 +11,30 @@ class App extends Component {
   state = {
     places : []
   }
+
+  retrieveVenues = () => {
+    const endPoint = 'https://api.foursquare.com/v2/venues/explore?';
+    let parameters = {
+      query : 'taxicab',
+      client_id : 'KGRM5HI3USNJX2LWHTWJ1J5PPJEF3F5LJP31GCLUATCK41AP',
+      client_secret : 'KKZWIDKWEICCGARKXIEZJFZOTJASTXJ33NUZJBM5QUYZO3AM',
+      v : '20183008',
+      near : 'mountain view'
+    }
+    axios.get(endPoint + new URLSearchParams(parameters)).then(response => {
+      this.setState({
+        places : response.data.response.groups[0].items
+      });
+      this.state.places.map(item => {
+       return console.log(item.venue.location)
+      });
+    }).catch(e => {
+      console.log(e)
+    })
+    console.log(parameters);
+    
+  }
+
   render() {
     return (
       <div className="App">
