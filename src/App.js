1b1876349ff73a1dd0f4a2cdeaf7bf9dccaeca17
Adel Lassag <adetech.aadl@gmail.com>
2018-08-30 20:35:50 +0100

    feat: Create Map component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Menu from './Menu';
 import Navbar from './Navbar';
+import Map from './Map';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <Menu />
+        <Map />
       </div>
     );
   }
