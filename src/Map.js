d5485130771bd5aff4128cd2aa3ebf77be2b92cd
Adel Lassag <adetech.aadl@gmail.com>
2018-08-31 00:10:20 +0100

    feat: Create map script

--- src/Map.js
+++ src/Map.js
@@ -4,6 +4,15 @@ import React, { Component } from 'react';
 
 class Map extends Component {
 
+  mapScript = (src) => {
+    let getFirstScript = window.document.getElementsByTagName('script')[0];
+    let creatMapScript = window.document.createElement('script');
+    creatMapScript.sync = true;
+    creatMapScript.defer = true;
+    creatMapScript.src = src;
+
+    getFirstScript.parentNode.insertBefore(creatMapScript, getFirstScript);
+  }
 
   render() {
     return (
