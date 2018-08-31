0e3ea5d302fddac7f2a920887a3946b3853eb5f6
Adel Lassag <adetech.aadl@gmail.com>
2018-08-31 13:52:30 +0100

    feat: Create update query methode

--- src/Menu.js
+++ src/Menu.js
@@ -9,30 +9,46 @@ class Menu extends Component {
         allPlaces : []
     }
     
-    
+
+    updateQuery = (query) => {
+        this.setState({ query })
+        
+        let allPlaces = this.props.places
+        let filtredPlaces
+
+        if(this.state.query && (this.state.query !== '')) {
+            const matches = new RegExp(escapeRegExp(query), 'i');
+            filtredPlaces = allPlaces.filter((place) => matches.test(place.venue.name))
+            this.setState({places: filtredPlaces})
+        } else {
+            this.setState({places: allPlaces})
+        }
+    }
 
     render() {
+
         
-      return (
-        <aside className="menu">
-            <h2 className="list-title">List of Taxicab services</h2>
-            <div className="search-box">
-                <input id="filter-input" autoFocus type="text" placeholder="Type to filter the list"></input>
-                <div id="filter-button">Filter</div>
-            </div>
-            <ul places={this.props.places}> 
-               
-               {
-                   this.props.places.map(place=> (
+        
+        return (
+            <aside className="menu">
+                <h2 className="list-title">List of Taxicab services</h2>
+                <div className="search-box">
+                    <input id="filter-input" autoFocus type="text" onChange={(event) => this.updateQuery(event.target.value)} placeholder="Type to filter the list"></input>
+                    <div id="filter-button">Filter</div>
+                </div>
+                <ul places={this.props.places}> 
+                
+                {
+                    this.props.places.map(place=> (
 
-                       <li>{place.venue.name}</li>
-                                           
-                   ))
+                        <li>{place.venue.name}</li>
+                                            
+                    ))
 
-               } 
-            </ul>
-        </aside>
-      );
+                } 
+                </ul>
+            </aside>
+        );
     }
 }
   
