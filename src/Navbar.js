538b4867120c83604aefb0ec7b2f2bef363445e0
Adel Lassag <adetech.aadl@gmail.com>
2018-09-02 02:06:02 +0100

    feat: Add toggle collapse menu funcionality

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
+import Menu from './Menu' 
 
 class Navbar extends Component {
+  toggleCollapse =() => {
+    let menu = document.getElementsByClassName('menu')[0];
+    if(menu.style.display === 'none') {
+      menu.style.display = 'block'
+    } else {
+      menu.style.display = 'none'
+    }
+  }
   render() {
     return (
       <header id="nav-bar">
         <h1 id="app-title">Taxicab services in NEW YORK</h1>
-        <img id="logo" src={logo}></img>
+        <img onClick={() => this.toggleCollapse()} id="logo" src={logo}></img>
       </header>
     )
   }
