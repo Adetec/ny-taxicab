5ca60ced465fed0d8848e22f220dcb77da653fef
Adel Lassag <adetech.aadl@gmail.com>
2018-09-02 06:48:44 +0100

    refactor: clean code and improve indendation

--- src/Menu.js
+++ src/Menu.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import escapeRegExp from 'escape-string-regexp';
 
 class Menu extends Component {
-    
+
     state = {
         query : '',
         filtredPlaces : [],
@@ -31,7 +31,7 @@ class Menu extends Component {
         document.getElementById('filter-input').value = '';
         this.updateQuery('')
     }
-    
+
     // This will update the list when filter is applied
     updateQuery = (query) => {
         this.setState({ query, onLoad : false })// Update the query state and remove default locations list
@@ -44,7 +44,6 @@ class Menu extends Component {
             this.setState({filtredPlaces});//restore all places
             this.props.filterPlaces(filtredPlaces);//Call parent function to restore places
             this.setState({onLoad : true});//Display All places in the list
-            
         } else {
             filtredPlaces = allPlaces.filter((place) => matches.test(place.venue.name));// Check if query matches location names
             this.setState({filtredPlaces});// Add just places that matches
@@ -52,15 +51,14 @@ class Menu extends Component {
             this.props.markers.filter(marker => {// Loop through each marek to filter with places
                 filtredPlaces.map((place) => {// Loop through each place to check if that match
                     return marker.id === place.venue.id && marker.setVisible(true);// If true display its own marker
-                    
                 });
                 return marker;
             });
         }
     }
-    
+
     render() {
-        
+
         return (
             <aside tabIndex="1" aria-label="Places list menu" className="menu">
                 <h2 id="lst-title" className="list-title">List of Taxicab services</h2>
@@ -68,34 +66,30 @@ class Menu extends Component {
                     <input id="filter-input"  type="text" role="search" onChange={(event) => this.updateQuery(event.target.value)} placeholder="Type to filter the list"></input>
                     <div onClick={() => this.emptySearchQuery()} id="filter-button" role="button" aria-label="Reset fitltred query">x</div>
                 </div>
-                
+
                 {
                     // If query is empty or page is loaded, render all places
-                    this.state.onLoad === true && (                
+                    this.state.onLoad === true && (
                         <ul id="list" tabIndex="2" role="group" aria-labelledby="lst-title">
                             {
                                 this.props.places.map(place=> (
-
                                     <li role="menuitem" aria-labelledby="list"  onClick={()=>this.infoWin(place.venue.id)} key={place.venue.id}>{place.venue.name}</li>
-                                                        
                                 ))
-                            } 
+                            }
                         </ul>
                     )
                 }
 
                 {
                     // If query matches at least one place, render filtred list
-                    this.state.filtredPlaces.length > 0 && (                
+                    this.state.filtredPlaces.length > 0 && (
                         <div>
                             <ul id="list" tabIndex="2" role="group" aria-labelledby="lst-title">
                                 {
                                     this.state.filtredPlaces.map(place=> (
-                                        
                                         <li onClick={()=>this.infoWin(place.venue.id)} key={place.venue.id}>{place.venue.name}</li>
-                                        
                                     ))
-                                } 
+                                }
                             </ul>
                             {
                                 // If there are more then one place, display places in plural
@@ -109,7 +103,7 @@ class Menu extends Component {
                                     <div>{this.state.filtredPlaces.length} place of {this.props.places.length}</div>
                                 )
                             }
-                            
+
                         </div>
                     )
                 }
@@ -117,5 +111,5 @@ class Menu extends Component {
         );
     }
 }
-  
+
 export default Menu;
