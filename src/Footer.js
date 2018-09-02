973fbb9681980a1728c1eceb87a393f13ec3bc8b
Adel Lassag <adetech.aadl@gmail.com>
2018-09-02 04:44:15 +0100

    feat: Add Footer component

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+class Footer extends Component {
+  
+  render() {
+    return (
+      <footer id="footer">
+        <p></p>
+      </footer>
+    )
+  }
+}
+
+export default Footer
